94564020d3b354914b834e0d467f4d0195c41876
강찬영 <backgwa@icloud.com>
2024-10-15 17:53:17 +0900

    Python 버전 불일치 버그 수정

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from Detectify import *
 from setuptools import setup, find_packages
 
 setup(
-    name="Detectify",
-    version="1.0.0",
+    name="detectify-python",
+    version="1.0.1",
     author="KANG CHANYOUNG",
     author_email="backgwa@icloud.com",
-    description="Detectify",
+    description="Detectify - YOLOv11 based object detection framework",
     url="https://github.com/BackGwa/Detectify",
     install_requires=[
         "ultralytics==8.3.13"
     ],
     packages = find_packages(),
-    python_requires=">=3.11.10"
+    python_requires=">=3.9.0"
 )
