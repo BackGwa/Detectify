8ec3dadcf17e09c10648f3c6016aeb81988d46a0
BackGwa <backgwa@icloud.com>
2024-10-15 21:24:14 +0900

    인스턴스 생성 함수 구성

--- Detectify/predict.py
+++ Detectify/predict.py
@@ -1,5 +1,17 @@
+import os
 from ultralytics import YOLO
+from .logger import Logger
 
 class Predict:
     def __init__(self, model: str):
-        self.model = YOLO(model=model)
+        self.log = Logger()
+        
+        if not os.path.exists(model):
+            self.log.error(f"현재 경로에 '{model}'이 존재하지 않습니다.")
+
+        try:
+            self.log.alert(f"'{model}'을 불러오고 있습니다.")
+            self.model = YOLO(model=model)
+            self.log.success(f"'{model}'를 성공적으로 불러왔습니다.")
+        except Exception as ex:
+            self.log.error(f"'{model}'를 불러오던 중 문제가 발생했습니다.", ex)
